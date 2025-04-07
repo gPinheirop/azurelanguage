9c0667019b342ad0db00e50950ad40090c63e0ce
gPinheirop <gabrielpedreira_@hotmail.com>
2025-04-07 19:19:45 -0300

    :tada: Initial commit

---
+++ src/index.js
@@ -0,0 +1,69 @@
+"use strict";
+
+import {
+  AzureKeyCredential,
+  TextAnalysisClient,
+} from "@azure/ai-language-text";
+import { env } from "../env.js";
+
+// This example requires environment variables named "LANGUAGE_KEY" and "LANGUAGE_ENDPOINT"
+const key = env.LANGUAGE_KEY;
+const endpoint = env.LANGUAGE_ENDPOINT;
+
+//an example document for sentiment analysis and opinion mining
+const documents = [
+  {
+    text: "The food and service were unacceptable. The concierge was nice, however.",
+    id: "0",
+    language: "en",
+  },
+];
+
+async function main() {
+  console.log("=== Sentiment analysis and opinion mining sample ===");
+
+  const client = new TextAnalysisClient(endpoint, new AzureKeyCredential(key));
+
+  const results = await client.analyze("SentimentAnalysis", documents, {
+    includeOpinionMining: true,
+  });
+
+  for (let i = 0; i < results.length; i++) {
+    const result = results[i];
+    console.log(`- Document ${result.id}`);
+    if (!result.error) {
+      console.log(`\tDocument text: ${documents[i].text}`);
+      console.log(`\tOverall Sentiment: ${result.sentiment}`);
+      console.log("\tSentiment confidence scores:", result.confidenceScores);
+      console.log("\tSentences");
+      for (const {
+        sentiment,
+        confidenceScores,
+        opinions,
+      } of result.sentences) {
+        console.log(`\t- Sentence sentiment: ${sentiment}`);
+        console.log("\t  Confidence scores:", confidenceScores);
+        console.log("\t  Mined opinions");
+        for (const { target, assessments } of opinions) {
+          console.log(`\t\t- Target text: ${target.text}`);
+          console.log(`\t\t  Target sentiment: ${target.sentiment}`);
+          console.log(
+            "\t\t  Target confidence scores:",
+            target.confidenceScores
+          );
+          console.log("\t\t  Target assessments");
+          for (const { text, sentiment } of assessments) {
+            console.log(`\t\t\t- Text: ${text}`);
+            console.log(`\t\t\t  Sentiment: ${sentiment}`);
+          }
+        }
+      }
+    } else {
+      console.error(`\tError: ${result.error}`);
+    }
+  }
+}
+
+main().catch((err) => {
+  console.error("The sample encountered an error:", err);
+});
